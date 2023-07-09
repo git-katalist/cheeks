80d1b85dd540369fb38e64ccd0f34f4afaae7143
tsots <mosalatsotetsi@gmail.com>
2023-07-09 23:49:42 +0200

    first commit

---
+++ src/js/navLinks.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+// document.querySelectorAll(".nav-link").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//   })
+// );
+
+const navLinksEls = document.querySelectorAll(".nav-link");
+const windowPathname = window.location.pathname;
+
+navLinksEls.forEach((navLinkEl) => {
+  const navlinkPathname = new URL(navLinkEl.href).pathname;
+  if (windowPathname === navlinkPathname) {
+    navLinkEl.classList.add("active");
+  }
+});
+console.log(window.location.pathname);
