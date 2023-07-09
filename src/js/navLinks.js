2a42d09641eac3881dc219fa002f37f3689a827e
tsots <mosalatsotetsi@gmail.com>
2023-07-10 01:38:16 +0200

    console.log(href)

--- src/js/navLinks.js
+++ src/js/navLinks.js
@@ -27,4 +27,5 @@ document.querySelectorAll(".nav-link").forEach((link) => {
   if (link.href === window.location.href) {
     link.setAttribute("aria-current", "page");
   }
+  console.log(link.href, window.location.href);
 });
